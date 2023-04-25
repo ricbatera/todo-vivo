6e39f0bfb94b237a148de92f0a810ffee8da7ed7
Ricardo Alves Roberto <contato@rdrtech.com.br>
2023-04-25 18:44:36 -0300

    ajuste na URL para o backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     logRequest: true,
     traceRequest: true,
-    //URLSERVIDOR: 'https://spring-cloud-vivo-ricardo-todo-gateway.azuremicroservices.io/TAREFAS-API/tarefas/',
-    URLSERVIDOR: 'http://localhost:8080/tarefas/',
+    URLSERVIDOR: 'https://spring-cloud-vivo-ricardo-todo-gateway.azuremicroservices.io/TAREFAS-API/tarefas/',
+    //URLSERVIDOR: 'http://localhost:8080/tarefas/',
   };
