38e9956ff393903460a1c936cc3ccee478477129
Ricardo Alves Roberto <contato@rdrtech.com.br>
2023-04-25 18:42:40 -0300

    incluir nova tarefa add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListaTarefasComponent } from './lista-tarefas/lista-tarefas.component';
 import { NovaTarefaComponent } from './nova-tarefa/nova-tarefa.component';
 import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,10 @@ import {MatTableModule} from '@angular/material/table';
     HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nova-tarefa/nova-tarefa.component.ts
+++ src/app/nova-tarefa/nova-tarefa.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DatabaseServiceService } from '../services/database-service.service';
+import { Tarefa } from '../model/tarefa';
 
 @Component({
   selector: 'app-nova-tarefa',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-tarefa.component.css']
 })
 export class NovaTarefaComponent {
+  tarefa = "";
 
+  constructor(
+    private db:DatabaseServiceService
+  ){}
+
+
+  novaTarefa(){
+    if(this.tarefa != ""){
+      let novaTarefa: Tarefa = {
+        id: null,
+        tarefa: this.tarefa,
+        concluido: false
+      }
+      this.db.novaTarefa(novaTarefa).subscribe(res=>{
+        alert(`${res.tarefa} criada com sucesso!`)
+      })
+    } else{
+      alert("Digite uma tarefa.")
+    }
+  }
 }
